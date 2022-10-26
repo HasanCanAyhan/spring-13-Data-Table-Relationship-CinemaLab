11887c4254106686c94d1a02ceee79e25358cfdf
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-26 14:08:00 +0200

    Initial commit

---
+++ src/main/java/com/cydeo/spring06datatablerelationshipcinemalab/Spring06DataTableRelationshipCinemaLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring06datatablerelationshipcinemalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06DataTableRelationshipCinemaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring06DataTableRelationshipCinemaLabApplication.class, args);
+    }
+
+}
