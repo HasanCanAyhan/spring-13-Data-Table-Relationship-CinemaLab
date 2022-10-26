a5f58c83d409d0edb5868e417b51f8d307a2d4f1
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-26 14:38:47 +0200

    spring data table relationship - cinemaLab

---
+++ src/main/java/com/cydeo/entity/Movie.java
@@ -0,0 +1,45 @@
+package com.cydeo.entity;
+
+
+import com.cydeo.enums.Status;
+import com.cydeo.enums.Type;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Movie extends BaseEntity{
+
+    private String name;
+
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
+
+    private Integer duration;
+
+    @Column(columnDefinition = "text")// there is no limit if you put text
+    private String summary;
+
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+    joinColumns =@JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genreList; // third table bcs many to many
+
+
+
+}
